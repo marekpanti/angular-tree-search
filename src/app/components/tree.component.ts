25ebcc35c9d6d507621a045808ca8a2a2c320785
marekpanti@gmail.com <marek@injurymap.com>
2023-03-26 15:27:56 +0200

    feat: expanded tree

--- src/app/components/tree.component.ts
+++ src/app/components/tree.component.ts
@@ -13,35 +13,42 @@ export class TreeComponent {
   private tree = slovakiaTreeModel;
   public filteredTree: any = [slovakiaTreeModel];
 
-  treeControl = new NestedTreeControl<any>(node => node.children);
+  treeControl = new NestedTreeControl<any>((node) => node.children);
   dataSource = new MatTreeNestedDataSource<any>();
   search = new FormControl('');
 
   constructor() {
     this.dataSource.data = this.filteredTree;
+    this.treeControl.dataNodes = this.filteredTree;
+    this.treeControl.expandAll();
   }
 
-  hasChild = (_: number, node: any) => !!node.children && node.children.length > 0;
+  hasChild = (_: number, node: any) =>
+    !!node.children && node.children.length > 0;
 
   stackSearch() {
     const tree: any = this.tree;
     const target = this.search.value;
-    console.log(this.search);
-    console.log(target);
+
     let result = [];
+
+    // tree traversal - we create a stack out of our tree
+    // then we check for the target and at the same time we add to the stack children
     for (const stack = [tree]; stack.length; ) {
       const curr = stack.pop();
 
       if (curr.title === target) {
-        // return curr.label;
         result.push(curr);
       }
-      // console.log(curr);
       stack.push(...curr.children);
     }
-    console.log(result);
     this.filteredTree = result;
     this.dataSource.data = this.filteredTree;
     return result;
+  }
+
+  reset() {
+    this.filteredTree = [this.tree];
+    this.dataSource.data = this.filteredTree;
   }
 }
